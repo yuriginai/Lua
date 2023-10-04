1e4ba368587cc068c2be93f3288677ae4673bd4a
Yuriginai <knepomucenodeoliveira@gmail.com>
2023-10-04 19:23:22 -0300

    segundo commit

---
+++ soma_dos_quadrados.lua
@@ -0,0 +1,16 @@
+--PROGRAMA SOMA DOS QUADRADOS--
+--VAR--
+
+local a, b, c = int
+local s = int
+
+print ("Insira o primeiro valor: \n")
+a = io.read();
+print ("Insira o segundo valor: \n")
+b = io.read();
+print ("Insira o terceiro valor \n")
+c = io.read();
+
+s = a + b + c
+
+print ("A soma dos valores é igual à: ",s)
